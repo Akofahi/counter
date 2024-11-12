45133e84821080f6713350b3a0c0b6683634aae7
Akofahi <akofahi92@gmail.com>
2024-11-12 10:24:12 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [counter, setCounter] = useState(0);
+  const date = new Date();
+  date.setDate(date.getDate() + counter * step);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div style={{ display: "flex", gap: "2px" }}>
+        <button
+          onClick={() => {
+            setStep((s) => s - 1);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          -
+        </button>
+        <p>{step}</p>
+        <button
+          onClick={() => {
+            setStep((s) => s + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
+      <div style={{ display: "flex", gap: "2px" }}>
+        <button
+          onClick={() => {
+            setCounter((s) => s - 1);
+          }}
+        >
+          -
+        </button>
+        <p>{counter}</p>
+        <button
+          onClick={() => {
+            setCounter((s) => s + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
+      <p>
+        {counter !== 0
+          ? `${counter * step} days from Today is ${date}`
+          : `Today is ${date}`}
+      </p>
+    </>
   );
 }
 
